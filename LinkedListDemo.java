a8c21fdfe03a77ea2c8d1a2c833dd4f965f18cca
Md. Refayet Hossen <138612383+rifat3790@users.noreply.github.com>
2023-07-31 18:56:14 +0600

    Add files via upload
    added linked list demo

---
+++ LinkedListDemo.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<String> country = new LinkedList<String>();
+
+        country.add("Bangladesh");
+        country.add("India");
+        country.add("Pakistan");
+        country.add("USA");
+
+        System.out.println("Country Name: "+country);
+    }
+    
+}
