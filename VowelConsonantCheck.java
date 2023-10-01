3a40ab95d04c8c47c55bbecfcdcb63396c04cc39
Md. Refayet Hossen <138612383+rifat3790@users.noreply.github.com>
2023-10-02 00:12:36 +0600

    added vowel consonant check

---
+++ VowelConsonantCheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class VowelConsonantCheck {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter any Alphabet: ");
+        char ch = s.next().charAt(0);
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+            System.out.println("Vowel");
+        }
+        else {
+            System.out.println("Consonant");
+        }
+    }
+}
